18bf4c5a97e436fd6f2d26f86f57e24ffff77885
Vivek Satloniya <sathloniyavivek64@gmail.com>
2025-10-04 14:00:36 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import LandingPage from "./components/layout/LandingPage";
+
+export default function App() {
+  return <div className="bg-zinc-950">
+    <LandingPage />
+  </div>;
+}
---
+++ src/components/layout/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div className='w-screen h-dvh text-white'>
+    <h1 className='font-display'>hii</h1>
+    </div>
+  )
+}
+
+export default LandingPage
